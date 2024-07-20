d795f060a70da1d912c9d001e4e396b473282805
Sai1137 <117431614+Sai1137@users.noreply.github.com>
2024-07-20 21:07:38 +0530

    UI improvements in subcatlist and fixed few errors

--- lib/NewdefaultprofilePage/defaultpage&tabview.dart
+++ lib/NewdefaultprofilePage/defaultpage&tabview.dart
@@ -47,98 +47,6 @@ class _DefaultProfilePageState extends State<DefaultProfilePage>
     super.dispose();
   }
 
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   _tabController = TabController(length: 5, vsync: this);
-  //   _scrollController = ScrollController();
-  //   // _keys = List<GlobalKey>.generate(5, (index) => GlobalKey());
-
-  //   // var data = Provider.of<BusinessProfileProvider>(context, listen: false);
-  //   // data.businessProfileProvider(widget.keyy, widget.uid);
-
-  // WidgetsBinding.instance.addPostFrameCallback((_) {
-  //   var data = Provider.of<BusinessProfileProvider>(context, listen: false);
-  //   data.businessProfileProvider(widget.keyy, widget.uid);
-  // });
-
-  //   // var dataAsk = Provider.of<AskCommunityProvider>(context, listen: false);
-  //   // dataAsk.fetchAskCommunityData(widget.uid);
-
-  //   Future.delayed(Duration(seconds: 2), () {
-  //     var dataAsk = Provider.of<AskCommunityProvider>(context, listen: false);
-  //     dataAsk.fetchAskCommunityData(widget.uid);
-  //   });
-
-  //   var datacomments =
-  //       Provider.of<CommentSectionProvider>(context, listen: false);
-  //   datacomments.commentSectionProvider(widget.uid);
-
-  //   // Future.microtask(() {
-  //   //   var data = Provider.of<BusinessProfileProvider>(context, listen: false);
-  //   //   data.businessProfileProvider(widget.uid);
-
-  //   //   var dataAsk = Provider.of<AskCommunityProvider>(context, listen: false);
-  //   //   dataAsk.fetchAskCommunityData(widget.uid);
-
-  //   //   var datacomments =
-  //   //       Provider.of<CommentSectionProvider>(context, listen: false);
-
-  //   //   datacomments.commentSectionProvider(widget.uid);
-  //   // });
-
-  //   // _scrollController.addListener(() {
-  //   //   if (_scrollController.offset >= 2000) {
-  //   //     // _tabController.animateTo(1);
-
-  //   //     // Change to the second tab when the scroll offset is greater than 200
-  //   //     // } else if (_scrollController.offset >= 530) {     //////////600
-  //   //     //   _tabController.animateTo(2);
-  //   //     // } else if (_scrollController.offset >= 1230 &&
-  //   //     //     _scrollController.offset < 1630) {                     /////////// 980
-  //   //     //   _tabController.animateTo(3);
-  //   //     // } else if (_scrollController.offset >= 1630) {          /////////1780
-  //   //     //   _tabController.animateTo(4);
-  //   //     // } else if (_scrollController.offset >= 600 &&
-  //   //     //     _scrollController.offset < 900) {
-  //   //     //   _tabController.animateTo(2);
-  //   //     // } else if (_scrollController.offset >= 980) {
-  //   //     //   _tabController.animateTo(3);
-  //   //     // } else if (_scrollController.offset >= 1780) {
-  //   //     //   _tabController.animateTo(4);
-  //   //   } else {
-  //   //     // _tabController.animateTo(
-  //   //     //     _tabController.index); // Change to the first tab otherwise
-
-  //   //     _tabController.animateTo(0);
-  //   //   }
-  //   // });
-  //   _tabController.addListener(() {
-  //     if (_tabController.indexIsChanging) {
-  //       if (_tabController.index == 0) {
-  //         _scrollController.animateTo(1 * 20,
-  //             duration: Duration(seconds: 1), curve: Curves.ease);
-  //       } else if (_tabController.index == 1) {
-  //         _scrollController.animateTo(2 * 90,
-  //             duration: Duration(seconds: 1), curve: Curves.ease);
-  //       } else if (_tabController.index == 2) {
-  //         _scrollController.animateTo(
-  //           3 * 250,
-  //           duration: Duration(seconds: 1),
-  //           curve: Curves.ease,
-  //         );
-  //       } else if (_tabController.index == 3) {
-  //         _scrollController.animateTo(4 * 212,
-  //             duration: Duration(seconds: 1), curve: Curves.ease);
-  //       } else if (_tabController.index == 4) {
-  //         _scrollController.animateTo(5 * 280,
-  //             duration: Duration(seconds: 1), curve: Curves.ease);
-  //       } else if (_tabController.indexIsChanging) {
-  //         _tabController.animateTo(0);
-  //       }
-  //     }
-  //   });
-  // }
   @override
   void initState() {
     super.initState();
@@ -271,25 +179,26 @@ class _DefaultProfilePageState extends State<DefaultProfilePage>
                               child: Text(""),
                             )
                           : GestureDetector(
-                            onTap: (){
-                              print(data.businessProfileData!.businessUid);
-                              showSnackBar(context,data.businessProfileData!.businessUid);
-                            },
-                            child: Text(
+                              onTap: () {
+                                print(data.businessProfileData!.businessUid);
+                                showSnackBar(context,
+                                    data.businessProfileData!.businessUid);
+                              },
+                              child: Text(
                                 // "Hub Restaurant",
                                 // data.businessProfileData!.businessName.toString(),
                                 // Replace this line:
-                            // data.businessProfileData!.businessName.toString(),
-                            
-                            // With this line:
+                                // data.businessProfileData!.businessName.toString(),
+
+                                // With this line:
                                 data.businessProfileData?.businessName
                                         .toString() ??
                                     'Default Name',
-                            
-                                style:
-                                    TextStyle(color: Colors.white, fontSize: 17),
+
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17),
                               ),
-                          ),
+                            ),
                     ),
                     background:
                         (data.businessProfileData?.profileImageUrl != null &&
@@ -401,8 +310,8 @@ class _DefaultProfilePageState extends State<DefaultProfilePage>
                     child: Container(
                       height: 600,
                       color: secondaryColor5LightTheme,
-                      // child: NewShowRewviewPage(),
-                      child: NewShowRewviewPageTest(),
+                      child: NewShowRewviewPage(),
+                      // child: NewShowRewviewPageTest(),
                     ),
                   ),
                 ),
@@ -459,3 +368,315 @@ class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
     );
   }
 }
+
+// class _DefaultProfilePageState extends State<DefaultProfilePage>
+//     with TickerProviderStateMixin {
+//   late ScrollController _scrollController;
+//   late TabController _tabController;
+
+//   int _calculateSelectedIndex(double scrollPosition) {
+//     if (scrollPosition < 1 * 300) return 0; // Directions
+//     if (scrollPosition < 2 * 320) return 1; // Overview
+//     if (scrollPosition < 3 * 400) return 2; // Amenities
+//     if (scrollPosition < 4 * 4) return 3; // Reviews
+//     return 4; // Ask Your Community
+//   }
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _tabController = TabController(length: 5, vsync: this);
+//     _scrollController = ScrollController();
+
+//     _scrollController.addListener(() {
+//       int newIndex = _calculateSelectedIndex(_scrollController.position.pixels);
+//       if (_tabController.index != newIndex) {
+//         setState(() {
+//           _tabController.index = newIndex;
+//         });
+//       }
+//     });
+
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       var data = Provider.of<BusinessProfileProvider>(context, listen: false);
+//       data.businessProfileProvider(widget.keyy, widget.uid);
+//       var dataAsk = Provider.of<AskCommunityProvider>(context, listen: false)
+//           .fetchAskCommunityData(widget.uid);
+//     });
+
+//     var datacomments =
+//         Provider.of<CommentSectionProvider>(context, listen: false)
+//             .commentSectionProvider(widget.uid);
+//     var servicesData = Provider.of<ServicesProvider>(context, listen: false)
+//         .getMongoBusinessData(widget.uid);
+
+//     _tabController.addListener(() {
+//       if (_tabController.indexIsChanging) {
+//         switch (_tabController.index) {
+//           case 0:
+//             _scrollController.animateTo(1 * 20,
+//                 duration: Duration(seconds: 1), curve: Curves.ease);
+//             break;
+//           case 1:
+//             _scrollController.animateTo(2 * 90,
+//                 duration: Duration(seconds: 1), curve: Curves.ease);
+//             break;
+//           case 2:
+//             _scrollController.animateTo(3 * 250,
+//                 duration: Duration(seconds: 1), curve: Curves.ease);
+//             break;
+//           case 3:
+//             _scrollController.animateTo(4 * 212,
+//                 duration: Duration(seconds: 1), curve: Curves.ease);
+//             break;
+//           case 4:
+//             _scrollController.animateTo(5 * 280,
+//                 duration: Duration(seconds: 1), curve: Curves.ease);
+//             break;
+//           default:
+//             _tabController.animateTo(0);
+//         }
+//       }
+//     });
+//   }
+
+//   @override
+//   void dispose() {
+//     _scrollController.removeListener(() {});
+//     _scrollController.dispose();
+//     _tabController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var data = Provider.of<BusinessProfileProvider>(context);
+
+//     return Scaffold(
+//       body: data.isLoading
+//           ? Center(
+//               child: CircularProgressIndicator(
+//               color: tgDarkPrimaryColor,
+//             ))
+//           : CustomScrollView(
+//               controller: _scrollController,
+//               slivers: <Widget>[
+//                 SliverAppBar(
+//                   backgroundColor: tgPrimaryColor,
+//                   expandedHeight: 200.0,
+//                   floating: true,
+//                   pinned: true,
+//                   leading: IconButton(
+//                       onPressed: () {
+//                         Navigator.of(context).pop();
+//                       },
+//                       icon: Icon(
+//                         LineAwesomeIcons.angle_left,
+//                         color: tgPrimaryText,
+//                       )),
+//                   actions: [
+//                     Row(
+//                       children: [
+//                         RatingBar.builder(
+//                           initialRating:
+//                               Provider.of<CommentSectionProvider>(context)
+//                                   .averageRating,
+//                           minRating: 1,
+//                           direction: Axis.horizontal,
+//                           allowHalfRating: true,
+//                           itemCount: 5,
+//                           itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+//                           itemSize: 12,
+//                           itemBuilder: (context, _) => Icon(
+//                             Icons.star,
+//                             color: Colors.amber[700],
+//                             size: 20,
+//                           ),
+//                           onRatingUpdate: (rating) {
+//                             // print(rating);
+//                           },
+//                         ),
+//                         SizedBox(width: 2),
+//                         Text(
+//                           Provider.of<CommentSectionProvider>(context)
+//                               .averageRating
+//                               .toStringAsFixed(1),
+//                           style: TextStyle(color: Colors.white, fontSize: 14),
+//                         )
+//                       ],
+//                     ),
+//                     Divider(
+//                       color: Colors.grey[200],
+//                       thickness: 5,
+//                     ),
+//                   ],
+//                   flexibleSpace: FlexibleSpaceBar(
+//                     title: Padding(
+//                       padding: const EdgeInsets.only(right: 2),
+//                       child: data.isLoading
+//                           ? Center(
+//                               child: Text(""),
+//                             )
+//                           : GestureDetector(
+//                               onTap: () {
+//                                 print(data.businessProfileData!.businessUid);
+//                                 showSnackBar(context,
+//                                     data.businessProfileData!.businessUid);
+//                               },
+//                               child: Text(
+//                                 data.businessProfileData?.businessName
+//                                         .toString() ??
+//                                     'Default Name',
+//                                 style: TextStyle(
+//                                     color: Colors.white, fontSize: 17),
+//                               ),
+//                             ),
+//                     ),
+//                     background:
+//                         (data.businessProfileData?.profileImageUrl != null &&
+//                                 data.businessProfileData?.profileImageUrl
+//                                     .isNotEmpty)
+//                             ? Image.network(
+//                                 data.businessProfileData?.profileImageUrl,
+//                                 fit: BoxFit.cover,
+//                               )
+//                             : Image.network(
+//                                 "https://via.placeholder.com/150",
+//                                 fit: BoxFit.cover,
+//                               ),
+//                   ),
+//                 ),
+//                 SliverToBoxAdapter(
+//                   child: AdditionalInfoPage(),
+//                 ),
+//                 SliverPersistentHeader(
+//                   delegate: _SliverAppBarDelegate(
+//                     TabBar(
+//                       controller: _tabController,
+//                       isScrollable: true,
+//                       labelColor: Colors.black87,
+//                       unselectedLabelColor: secondaryColor20LightTheme,
+//                       indicator: UnderlineTabIndicator(
+//                           borderRadius: BorderRadius.all(Radius.circular(2)),
+//                           borderSide: BorderSide(color: tgPrimaryColor)),
+//                       tabs: [
+//                         Tab(text: "Directions"),
+//                         Tab(text: "Overview"),
+//                         Tab(text: "Amenities"),
+//                         Tab(text: "Reviews"),
+//                         Tab(text: "Ask Your Community"),
+//                       ],
+//                     ),
+//                   ),
+//                   pinned: true,
+//                 ),
+//                 SliverToBoxAdapter(
+//                   child: AutoScaleTabBarView(
+//                     controller: _tabController,
+//                     children: [
+//                       MapScreenPage(
+//                         businesses: data.businessProfileData != null
+//                             ? [data.businessProfileData!]
+//                             : [],
+//                       ),
+//                       if (data.businessProfileData != null &&
+//                           data.businessProfileData!.subCategory ==
+//                               "housingrent")
+//                         Container(
+//                           height: 7,
+//                         ),
+//                       if (data.businessProfileData == null ||
+//                           data.businessProfileData!.subCategory !=
+//                               "housingrent")
+//                         Container(
+//                           height: 7,
+//                         ),
+//                       Container(
+//                         height: 7,
+//                       ),
+//                       Container(
+//                         height: 7,
+//                       ),
+//                       Container(
+//                         height: 7,
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//                 SliverToBoxAdapter(
+//                   child: Padding(
+//                     padding: const EdgeInsets.symmetric(vertical: 16),
+//                     child: Container(
+//                       color: Colors.white,
+//                       child: OverviewPage(),
+//                     ),
+//                   ),
+//                 ),
+//                 SliverToBoxAdapter(
+//                   child: Container(
+//                     color: Colors.white,
+//                     child: AmenitiesGridView(),
+//                   ),
+//                 ),
+//                 SliverToBoxAdapter(
+//                   child: Padding(
+//                     padding: const EdgeInsets.symmetric(vertical: 16),
+//                     child: Container(
+//                       height: 600,
+//                       color: secondaryColor5LightTheme,
+//                       child: NewShowRewviewPage(),
+//                     ),
+//                   ),
+//                 ),
+//                 SliverToBoxAdapter(
+//                   child: Padding(
+//                     padding: const EdgeInsets.symmetric(vertical: 16),
+//                     child: Container(
+//                         height: 600,
+//                         color: secondaryColor5LightTheme,
+//                         child: AskForCommunityWidget(
+//                           uid: '',
+//                           Questionid: "",
+//                         )),
+//                   ),
+//                 ),
+//                 SliverToBoxAdapter(
+//                   child: Padding(
+//                     padding: const EdgeInsets.symmetric(vertical: 16),
+//                     child: Container(
+//                         height: 600,
+//                         color: secondaryColor5LightTheme,
+//                         child: Container()),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//     );
+//   }
+// }
+
+// class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+//   _SliverAppBarDelegate(this._tabBar);
+
+//   final TabBar _tabBar;
+
+//   @override
+//   double get maxExtent => _tabBar.preferredSize.height;
+
+//   @override
+//   double get minExtent => _tabBar.preferredSize.height;
+
+//   @override
+//   bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+//     return false;
+//   }
+
+//   @override
+//   Widget build(
+//       BuildContext context, double shrinkOffset, bool overlapsContent) {
+//     return new Container(
+//       color: Colors.white,
+//       child: _tabBar,
+//     );
+//   }
+// }
